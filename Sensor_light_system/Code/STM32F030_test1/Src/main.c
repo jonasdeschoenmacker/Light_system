bb26e3ce3106062e7b95133c8c6a3d97a83d4dfc
Jonas De Schoenmacker <jonas.deschoenmacker@student.odisee.be>
2019-10-28 12:44:47 +0100

    main.c sensor aangepast

--- Sensor_light_system/Code/STM32F030_test1/Src/main.c
+++ Sensor_light_system/Code/STM32F030_test1/Src/main.c
@@ -104,39 +104,48 @@ int main(void)
     /* USER CODE END WHILE */
 	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)==GPIO_PIN_SET){
 		//sensor1 detect
-		HAL_I2C_Master_Transmit (&hi2c1, 0x4E,(uint8_t *) 0x08, 1, 100);
-
+		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x01, 1 , 100);
 
 	}
 	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1)==GPIO_PIN_SET){
 		//sensor2 detect
+		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x02, 1 , 100);
 	}
 	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2)==GPIO_PIN_SET){
 		//sensor3 detect
+		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x03, 1 , 100);
 	}
 	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3)==GPIO_PIN_SET){
 		//sensor4 detect
+		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x04, 1 , 100);
 	}
 	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4)==GPIO_PIN_SET){
 		//sensor5 detect
+		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x05, 1 , 100);
 	}
 	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5)==GPIO_PIN_SET){
 		//sensor6 detect
+		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x06, 1 , 100);
 	}
 	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_6)==GPIO_PIN_SET){
 		//sensor7 detect
+		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x07, 1 , 100);
 	}
 	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_7)==GPIO_PIN_SET){
 		//sensor8 detect
+		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x08, 1 , 100);
 	}
 	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8)==GPIO_PIN_SET){
 		//sensor9 detect
+		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x09, 1 , 100);
 	}
 	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9)==GPIO_PIN_SET){
 		//sensor10 detect
+		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x0A, 1 , 100);
 	}
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, false);
 
-
+	HAL_Delay(1000);
 
     /* USER CODE BEGIN 3 */
   }
@@ -200,7 +209,7 @@ static void MX_I2C1_Init(void)
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
   hi2c1.Init.Timing = 0x2000090E;
-  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.OwnAddress1 = 0x01;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
   hi2c1.Init.OwnAddress2 = 0;
