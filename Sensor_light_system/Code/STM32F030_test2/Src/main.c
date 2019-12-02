fb6ba46958b3c3d4c346c3f1a6264ec8213d6723
Jonas De Schoenmacker <jonas.deschoenmacker@student.odisee.be>
2019-12-02 12:21:18 +0100

    code aangepast

--- Sensor_light_system/Code/STM32F030_test2/Src/main.c
+++ Sensor_light_system/Code/STM32F030_test2/Src/main.c
@@ -21,11 +21,20 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f0xx_hal.h"
-
+#include <stdbool.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+  typedef enum status{
+	  init = 0,
+	  sending = 1,
+	  sended = 2,
+	  running = 3
+  };
 
+  bool data_old[10]={0,0,0,0,0,0,0,0,0,0};
+  bool data_new[10]={0,0,0,0,0,0,0,0,0,0};
+  bool flags[10]={0,0,0,0,0,0,0,0,0,0};
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -54,6 +63,7 @@ I2C_HandleTypeDef hi2c1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_TIM6_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -80,6 +90,10 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_TIM6_Init();
+  HAL_TIM_Base_Init(htim6);
 
   /* USER CODE END Init */
 
@@ -91,10 +105,11 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C1_Init();
+
   /* USER CODE BEGIN 2 */
 
+
+  //HAL_TIM_PeriodElapsedCallback(htim6);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -102,56 +117,58 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)==GPIO_PIN_SET){
-		//sensor1 detect
-		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x01, 1 , 100);
+	  status=init;
+	  while(status==init){
+		  updateLED();
+		  if(HAL_I2C_IsDeviceReady(&hi2c1, 0xFF, 10, 100)==HAL_OK){
+			  HAL_TIM_Base_Start(htim6);
+			  status=running;
+		  }
 
-	}
-	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1)==GPIO_PIN_SET){
-		//sensor2 detect
-		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x02, 1 , 100);
-	}
-	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2)==GPIO_PIN_SET){
-		//sensor3 detect
-		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x03, 1 , 100);
-	}
-	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3)==GPIO_PIN_SET){
-		//sensor4 detect
-		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x04, 1 , 100);
-	}
-	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4)==GPIO_PIN_SET){
-		//sensor5 detect
-		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x05, 1 , 100);
-	}
-	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5)==GPIO_PIN_SET){
-		//sensor6 detect
-		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x06, 1 , 100);
-	}
-	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_6)==GPIO_PIN_SET){
-		//sensor7 detect
-		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x07, 1 , 100);
-	}
-	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_7)==GPIO_PIN_SET){
-		//sensor8 detect
-		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x08, 1 , 100);
-	}
-	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8)==GPIO_PIN_SET){
-		//sensor9 detect
-		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x09, 1 , 100);
-	}
-	if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9)==GPIO_PIN_SET){
-		//sensor10 detect
-		HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x0A, 1 , 100);
-	}
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, false);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, false); 				//blauwe led uitzetten
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, false); 				//groene led uitzetten
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, false); 				//rode led uitzetten
+		HAL_Delay(100);
+	  }
+
+
 
 	HAL_Delay(1000);
+	status=running;
+	updateLED();
 
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
 
+void updateLED(void){
+
+	switch(status){
+	case init:
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, true); 				//blauwe led uitzetten
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, false); 				//groene led aanzetten
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, false); 				//rode led uitzetten
+		break;
+	case sending:
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, false); 				//blauwe led uitzetten
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, false); 				//groene led aanzetten
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, true); 				//rode led uitzetten
+		break;
+	case sended:
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, false); 				//blauwe led uitzetten
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, true); 				//groene led aanzetten
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, true); 				//rode led uitzetten
+		break;
+	case running:
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, false); 				//blauwe led uitzetten
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, true); 				//groene led aanzetten
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, false); 				//rode led uitzetten
+		break;
+	}
+
+
+}
 /**
   * @brief System Clock Configuration
   * @retval None
@@ -276,7 +293,109 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
+/**
+  * @brief TIM6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM6_Init(void)
+{
 
+  /* USER CODE BEGIN TIM6_Init 0 */
+
+  /* USER CODE END TIM6_Init 0 */
+
+  /* USER CODE BEGIN TIM6_Init 1 */
+
+  /* USER CODE END TIM6_Init 1 */
+  htim6.Instance = TIM6;
+  htim6.Init.Prescaler = 800;
+  htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim6.Init.Period = 1000;
+  htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM6_Init 2 */
+
+  /* USER CODE END TIM6_Init 2 */
+
+}
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+	if(htim->Instance==TIM6){
+		status=sending;
+		updateLED();
+		for(int i = 0; i<9; i++){
+			data_old[i]=data_new[i];
+			data_new[i]=0;
+
+			if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_i)==GPIO_PIN_SET){
+				//sensor detect
+				data_new[i]=1;
+			}
+			if(data_old[i] == data_new[i]){
+				flags[i]= 0;
+			}else{
+				if(data_new[j]){
+					HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) j+1, 1 , 100);
+				}else HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) j+241, 1 , 100);
+				flags[i]= 1;
+			}
+		}
+		status=sended;
+		updateLED();
+
+		/*for(uint_8t j = 0; j<9; j++){
+			if(flags[j]){
+				if(data_new[j]){
+					HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) j+1, 1 , 100);
+				}else HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) j+241, 1 , 100);
+			}
+		}*/
+
+		/*if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)==GPIO_PIN_SET){
+				//sensor1 detect
+				HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x01, 1 , 100);
+			}
+			if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1)==GPIO_PIN_SET){
+				//sensor2 detect
+				HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x02, 1 , 100);
+			}
+			if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2)==GPIO_PIN_SET){
+				//sensor3 detect
+				HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x03, 1 , 100);
+			}
+			if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3)==GPIO_PIN_SET){
+				//sensor4 detect
+				HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x04, 1 , 100);
+			}
+			if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4)==GPIO_PIN_SET){
+				//sensor5 detect
+				HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x05, 1 , 100);
+			}
+			if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5)==GPIO_PIN_SET){
+				//sensor6 detect
+				HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x06, 1 , 100);
+			}
+			if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_6)==GPIO_PIN_SET){
+				//sensor7 detect
+				HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x07, 1 , 100);
+			}
+			if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_7)==GPIO_PIN_SET){
+				//sensor8 detect
+				HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x08, 1 , 100);
+			}
+			if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8)==GPIO_PIN_SET){
+				//sensor9 detect
+				HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x09, 1 , 100);
+			}
+			if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9)==GPIO_PIN_SET){
+				//sensor10 detect
+				HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t *) 0x0A, 1 , 100);
+			}*/
+	}
+}
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
